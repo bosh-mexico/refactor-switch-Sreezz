36c95430e72fee16ee16e4c8e220d866aab4ee27
Sreezz <65155642+Sreezz@users.noreply.github.com>
2025-09-30 09:28:23 +0530

    Create checkout.h

---
+++ checkout.h
@@ -0,0 +1,15 @@
+#ifndef CHECKOUT_H_
+#define CHECKOUT_H_
+
+// Enum for Payment Modes
+typedef enum {
+    PayPal,
+    GooglePay,
+    CreditCard,
+    Unknown,
+    PaymentModeCount
+} PaymentMode;
+
+void checkout(PaymentMode mode, double amount);
+
+#endif  // STRINGCALCULATOR_H_
